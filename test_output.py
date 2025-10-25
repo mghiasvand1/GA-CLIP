c4d761cae4ef3b24cbe82e56a088a5e5e140395e
Mohammad Ghiasvand Mohammadkhani <178411493+mghiasvand1@users.noreply.github.com>
2025-10-25 09:48:44 +0330

    Update test_output.py

--- test_output.py
+++ test_output.py
@@ -10,14 +10,13 @@ def eval_text_pivot(ds, a="1.0"):
         tot += 1
         x = inference([ex["image_0"], ex["image_1"]], ex["caption_0"], "text", [a])
         y = inference([ex["image_0"], ex["image_1"]], ex["caption_1"], "text", [a])
-        if x and y:
-            x, y = (
-                x[a] if isinstance(x, dict) else x,
-                y[a] if isinstance(y, dict) else y,
-            )
-            if torch.argmax(x) == 0 and torch.argmax(y) == 1:
-                cor += 1
-    print(f"Text-pivot acc: {100*cor/tot:.2f}%")
+        x, y = (
+            x[a] if isinstance(x, dict) else x,
+            y[a] if isinstance(y, dict) else y,
+        )
+        if torch.argmax(x) == 0 and torch.argmax(y) == 1:
+            cor += 1
+    print(f"{100*cor/tot:.2f}%")
 
 
 def eval_image_pivot(ds, a="1.0"):
@@ -26,14 +25,13 @@ def eval_image_pivot(ds, a="1.0"):
         tot += 1
         x = inference(ex["image_0"], [ex["caption_0"], ex["caption_1"]], "image", [a])
         y = inference(ex["image_1"], [ex["caption_0"], ex["caption_1"]], "image", [a])
-        if x and y:
-            x, y = (
-                x[a] if isinstance(x, dict) else x,
-                y[a] if isinstance(y, dict) else y,
-            )
-            if torch.argmax(x) == 0 and torch.argmax(y) == 1:
-                cor += 1
-    print(f"Image-pivot acc: {100*cor/tot:.2f}%")
+        x, y = (
+            x[a] if isinstance(x, dict) else x,
+            y[a] if isinstance(y, dict) else y,
+        )
+        if torch.argmax(x) == 0 and torch.argmax(y) == 1:
+            cor += 1
+    print(f"{100*cor/tot:.2f}%")
 
 
 ds = load_dataset("facebook/winoground", split="test")
